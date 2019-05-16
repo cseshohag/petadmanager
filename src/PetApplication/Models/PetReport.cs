2f6d7bca0d1aa7a7f45b794d71d5cc25f3502150
cseshohag <solaiman.shohag95@gmail.com>
2019-05-16 10:48:43 +0600

    Add petReport model class and dBSet

---
+++ src/PetApplication/Models/PetReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PetApplication.Models
+{
+    public class PetReport
+    {
+        [Key]
+        public int ReportID { get; set; }
+
+        public int Id { get; set; }
+        public string Report { get; set; }
+
+        public virtual PetAnimal PetAminal { get; set; }
+        
+    }
+}
